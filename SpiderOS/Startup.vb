59f855f75fa9b742e49600e3070f05e5bc711553
Tomasz Ciupa <tomekciupa07.02@wp.pl>
2019-01-14 23:08:02 +0100

    .

--- SpiderOS/Startup.vb
+++ SpiderOS/Startup.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Startup
     Private Sub Startup_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         LoginForm.Show()
-        Hide()
+        Me.Close();
     End Sub
 End Class
