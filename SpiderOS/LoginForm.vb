375f805b48e83650d0cde50bc9be55a50ef0f994
Tomasz Ciupa <tomekciupa07.02@wp.pl>
2019-01-14 23:10:57 +0100

    .

--- SpiderOS/LoginForm.vb
+++ SpiderOS/LoginForm.vb
@@ -1,3 +1,5 @@
 ﻿Public Class LoginForm
-
+    Private Sub LoginForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Startup.Close()
+    End Sub
 End Class
--- SpiderOS/Startup.vb
+++ SpiderOS/Startup.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Startup
     Private Sub Startup_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         LoginForm.Show()
-        Me.Close()
+
+
     End Sub
 End Class
