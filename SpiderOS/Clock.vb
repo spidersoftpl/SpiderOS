fe57a7ae3d32a4c7c70fffd581e8534b9284d79c
Tomasz Ciupa <tomekciupa07.02@wp.pl>
2019-01-19 00:33:39 +0100

    .

--- SpiderOS/Clock.vb
+++ SpiderOS/Clock.vb
@@ -1,14 +1,16 @@
 ﻿Public Class Clock
+
     Private Sub Clock_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Timer1.Start()
+
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Label1.Text = DateTime.Now.ToString("HH:mm")
-        ProgressBar1.Increment(1)
-        If ProgressBar1.Value = 60 Then
-            ProgressBar1.Value = 0
+        Dim SecondString As String = DateTime.Now.ToString("ss")
+        Dim Second As Int64 = Convert.ToInt64(SecondString)
 
-        End If
     End Sub
+
+    Public ReadOnly Property Second As Integer
 End Class
