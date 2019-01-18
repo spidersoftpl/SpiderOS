696997e5499fd2d7e17be60de7c29dfd0a724dc3
Tomasz Ciupa <tomekciupa07.02@wp.pl>
2019-01-19 00:13:23 +0100

    .

--- SpiderOS/Clock.vb
+++ SpiderOS/Clock.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Clock
     Private Sub Clock_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        Timer1.Start()
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
