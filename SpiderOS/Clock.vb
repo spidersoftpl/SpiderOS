4f822584e8cc55a93a0698ff0013c57d142dfe55
Tomasz Ciupa <tomekciupa07.02@wp.pl>
2019-01-19 00:04:50 +0100

    .

--- SpiderOS/Clock.vb
+++ SpiderOS/Clock.vb
@@ -4,7 +4,7 @@
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        Label1.Text = TimeOfDay.ToString("hh:mm")
+        Label1.Text = TimeString.ToString("hh:mm")
 
 
     End Sub
