cfe76924fff1c472b6daa14ef7967aa479a1d96f
Tomasz Ciupa <tomekciupa07.02@wp.pl>
2019-01-27 00:24:32 +0100

    .

--- SpiderOS/LoginForm.Designer.vb
+++ SpiderOS/LoginForm.Designer.vb
@@ -66,7 +66,7 @@ Partial Class LoginForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.CadetBlue
+        Me.BackColor = System.Drawing.Color.DarkTurquoise
         Me.ClientSize = New System.Drawing.Size(1264, 681)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
